262888b46b20ed75b451bd3dfe87f3c027e4f756
Ural Ataullin <ataullinural@gmail.com>
2023-06-21 21:30:30 +0500

    secret key default

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-cg6')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6')
 
 DEBUG = bool(os.getenv('DEBUG'))
 
