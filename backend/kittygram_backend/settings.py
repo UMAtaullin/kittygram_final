e4b550ae43fc14a0773e17063c3ce9ac51c0ceea
Ural Ataullin <ataullinural@gmail.com>
2023-06-21 14:46:49 +0500

    SECRET KEY

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ['158.160.7.223', '127.0.0.1', 'localhost', 'umataullin.dynnamn.ru']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost']).split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
