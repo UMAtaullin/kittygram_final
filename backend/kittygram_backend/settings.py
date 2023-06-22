c08085428e6adaa32a0db2ec8a14326ce29d0a24
Ural Ataullin <ataullinural@gmail.com>
2023-06-22 12:03:56 +0500

    debag

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 # SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-# DEBUG = bool(os.getenv('DEBUG'))
-DEBAG = True
+DEBUG = bool(os.getenv('DEBUG'))
+# DEBAG = True
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 ALLOWED_HOSTS = ['158.160.7.223', '127.0.0.1', 'localhost', 'umataullin.dynnamn.ru']
