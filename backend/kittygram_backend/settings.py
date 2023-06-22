f29a2aba5f44a35a5e711e22459b857a5ae3ddd6
Ural Ataullin <ataullinural@gmail.com>
2023-06-22 08:59:38 +0500

    deploy

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,11 +6,14 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
+# SECRET_KEY = "os.getenv('SECRET_KEY', 'default_key')"
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = bool(os.getenv('DEBUG'))
+# DEBUG = bool(os.getenv('DEBUG'))
+DEBAG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
